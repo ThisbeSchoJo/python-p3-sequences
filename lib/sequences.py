5e81861941e00b459fa251f8717076fba4fae765
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-19 15:26:42 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # adds current number to previous number
+    # adds i to i-1
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length ==2:
+        print([0, 1])
+    else:
+        fib_sequence = [0,1]
+        for i in range(2, length):
+            next_num = fib_sequence[-2]+fib_sequence[-1]
+            fib_sequence.append(next_num)
+        print(fib_sequence)
+
+
